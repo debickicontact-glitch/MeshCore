80d3acbf24afe92a3021c95ef1b917a698e38e50
debickicontact-glitch <debicki.contact@gmail.com>
2026-09-20 13:02:06 +0200

    Update SerialBLEInterface.cpp

--- src/helpers/esp32/SerialBLEInterface.cpp
+++ src/helpers/esp32/SerialBLEInterface.cpp
@@ -96,6 +96,7 @@ void SerialBLEInterface::onConnect(BLEServer* pServer) {
 void SerialBLEInterface::onConnect(BLEServer* pServer, esp_ble_gatts_cb_param_t *param) {
   BLE_DEBUG_PRINTLN("onConnect(), conn_id=%d, mtu=%d", param->connect.conn_id, pServer->getPeerMTU(param->connect.conn_id));
   last_conn_id = param->connect.conn_id;
+  deviceConnected = true;
 }
 
 void SerialBLEInterface::onMtuChanged(BLEServer* pServer, esp_ble_gatts_cb_param_t* param) {
